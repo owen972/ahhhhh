fc11298d8abbcbab102ffc526bed7f02afe77b35
Owen Hilkemeijer <150102010+owen972@users.noreply.github.com>
2024-05-14 09:05:22 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -23,76 +23,76 @@ controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     mySprite,
     [img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . f 2 f e e e e f f . . . 
-        . . . f 2 2 2 f e e e e f f . . 
-        . . . f e e e e f f e e e f . . 
-        . . f e 2 2 2 2 e e f f f f . . 
-        . . f 2 e f f f f 2 2 2 e f . . 
-        . . f f f e e e f f f f f f f . 
-        . . f e e 4 4 f b e 4 4 e f f . 
-        . . f f e d d f 1 4 d 4 e e f . 
-        . f d d f d d d d 4 e e e f . . 
-        . f b b f e e e 4 e e f . . . . 
-        . f b b e d d 4 2 2 2 f . . . . 
-        . . f b e d d e 4 4 4 f f . . . 
-        . . . f f e e f f f f f f . . . 
-        . . . . f f f . . . f f . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f f f f d d d d d e e f . . 
+        . f d d d d f 4 4 4 e e f . . . 
+        . f b b b b f 2 2 2 2 f 4 e . . 
+        . f b b b b f 2 2 2 2 f d 4 . . 
+        . . f c c f 4 5 5 4 4 f 4 4 . . 
+        . . . f f f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
         `,img`
         . . . . . . . . . . . . . . . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . f 2 f e e e e f f . . . 
-        . . . f 2 2 2 f e e e e f f . . 
-        . . . f e e e e f f e e e f . . 
-        . . f e 2 2 2 2 e e f f f f . . 
-        . . f 2 e f f f f 2 2 2 e f . . 
-        . . f f f e e e f f f f f f f . 
-        . . f e e 4 4 f b e 4 4 e f f . 
-        . . . f e d d f 1 4 d 4 e e f . 
-        . . . . f d d d e e e e e f . . 
-        . . . . f e 4 e d d 4 f . . . . 
-        . . . . f 2 2 e d d e f . . . . 
-        . . . f f 5 5 f e e f f f . . . 
-        . . . f f f f f f f f f f . . . 
-        . . . . f f f . . . f f . . . . 
-        `,img`
-        . . . . . f f f f f f . . . . . 
-        . . . . f 2 f e e e e f f . . . 
-        . . . f 2 2 2 f e e e e f f . . 
-        . . . f e e e e f f e e e f . . 
-        . . f e 2 2 2 2 e e f f f f . . 
-        . . f 2 e f f f f 2 2 2 e f . . 
-        . . f f f e e e f f f f f f f . 
-        . . f e e 4 4 f b e 4 4 e f f . 
-        . . f f e d d f 1 4 d 4 e e f . 
-        . f d d f d d d d 4 e e e f . . 
-        . f b b f e e e 4 e e f f . . . 
-        . f b b e d d 4 2 2 2 f . . . . 
-        . . f b e d d e 2 2 2 e . . . . 
-        . . . f f e e f 4 4 4 f . . . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . . . f f f . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . f f e 2 f f f f f f 2 e f f . 
+        . f f f f f e e e e f f f f f . 
+        . . f e f b f 4 4 f b f e f . . 
+        . f f e 4 1 f d d f 1 4 e f . . 
+        f d f f e 4 d d d d 4 e f e . . 
+        f b f e f 2 2 2 2 e d d 4 e . . 
+        f b f 4 f 2 2 2 2 e d d e . . . 
+        f c f . f 4 4 5 5 f e e . . . . 
+        . f f . f f f f f f f . . . . . 
+        . . . . f f f . . . . . . . . . 
         `,img`
         . . . . . . . . . . . . . . . . 
-        . . . . f f f f f f . . . . . . 
-        . . . f 2 f e e e e f f . . . . 
-        . . f 2 2 2 f e e e e f f . . . 
-        . . f e e e e f f e e e f . . . 
-        . f e 2 2 2 2 e e f f f f . . . 
-        . f 2 e f f f f 2 2 2 e f . . . 
-        . f f f e e e f f f f f f f . . 
-        . f e e 4 4 f b e 4 4 e f f . . 
-        . . f e d d f 1 4 d 4 e e f . . 
-        . . . f d d d e e e e e f . . . 
-        . . . f e 4 e d d 4 f . . . . . 
-        . . . f 2 2 e d d e f . . . . . 
-        . . f f 5 5 f e e f f f . . . . 
-        . . f f f f f f f f f f . . . . 
-        . . . f f f . . . f f . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f e e 2 2 2 2 2 2 e f f . . 
+        . f f e 2 f f f f f f 2 e f f . 
+        . f f f f f e e e e f f f f f . 
+        . . f e f b f 4 4 f b f e f . . 
+        . . f e 4 1 f d d f 1 4 e f . . 
+        . . e f f f f d d d 4 e f . . . 
+        . . f d d d d f 2 2 2 f e f . . 
+        . . f b b b b f 2 2 2 f 4 e . . 
+        . . f b b b b f 5 4 4 f . . . . 
+        . . . f c c f f f f f f . . . . 
+        . . . . f f . . . f f f . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f e e 2 2 2 2 2 2 e f f . . 
+        . f f e 2 f f f f f f 2 e f f . 
+        . f f f f f e e e e f f f f f . 
+        . . f e f b f 4 4 f b f e f . . 
+        . . f e 4 1 f d d f 1 4 e f f . 
+        . . e f e 4 d d d d 4 e f f d f 
+        . . e 4 d d e 2 2 2 2 f e f b f 
+        . . . e d d e 2 2 2 2 f 4 f b f 
+        . . . . e e f 5 5 4 4 f . f c f 
+        . . . . . f f f f f f f . f f . 
+        . . . . . . . . . f f f . . . . 
         `],
     500,
-    true
+    false
     )
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
